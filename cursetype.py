0c2dc204a57b8182ed0a4ae873f7d767d222c41b
ianshapiro1 <ianshapiro1@gmail.com>
2023-10-03 21:29:38 -0500

    paragraph mode

--- cursetype.py
+++ cursetype.py
@@ -6,27 +6,23 @@ with open('1-1000.txt', 'r') as f:
     w1000 = f.read().splitlines()
 words_by_num = [[word for word in w1000 if len(word) == i] for i in range(1, 12)]
 
-def generate_sentence(l):
-    words = [f'{random.choice(w1000)}' for i in range(l)]
-    sentence = ' '.join(words)
-    return sentence
 
-def generate_paragraph(l):
-    words = [f'{random.choice(w1000)}' for i in range(l*12)]
-    paragraph = ' '.join(words)
-    firstthird = len(paragraph)//3
-    secondthird = firstthird + firstthird
-    stop1, stop2 = 0, 0
-    for i in range(len(paragraph)):
-        if i >= firstthird:
-            if paragraph[i:i+1] == ' ':
-                stop1 = i
-                firstthird = len(paragraph)+1
-        elif i >= secondthird:
-            if paragraph[i:i+1] == ' ':
-                stop2 = i
-                break
-    return  paragraph, stop1, stop2
+def generate_sentence(l):
+    sentence = []
+    slen = 0
+    while True:
+        word = random.choice(w1000)
+        if slen + len(word)+1 < l:
+            sentence.append(word)
+            slen += len(word)+1
+        elif slen + len(word)+1 > l:
+            return ' '.join(sentence)
+            break
+        elif slen + len(word)+1 == l:
+            sentence.append(word)
+            slen += len(word)+1
+            return ' '.join(sentence)
+            break
 
 def main(window):
     #INIT
@@ -71,8 +67,9 @@ def main(window):
         counter.refresh()
     
     def sentence_mode():
+        length = 80
         y, x = (window.getmaxyx()[0]//2, window.getmaxyx()[1]//2)
-        sentence = generate_sentence(random.randint(10, 15))
+        sentence = generate_sentence(length)
         stops = [0 for i in sentence]
         lletters = [chr(i) for i in range(97, 123)]
         uletters = [chr(i) for i in range(65, 91)]
@@ -167,13 +164,18 @@ def main(window):
 
     def paragraph_mode():
         #INIT
+        length = 80
         y, x = (window.getmaxyx()[0]//3, window.getmaxyx()[1]//2)
-        sentence = generate_paragraph(3)
-        stops = [0 for i in sentence]
+        sentence = generate_sentence(length)
+        s1 = generate_sentence(length)
+        s2 = generate_sentence(length)
+        sentences = [sentence, s1, s2]
+        stops = [[0 for i in sentence], [0 for i in s1], [0 for i in s2]]
         lletters = [chr(i) for i in range(97, 123)]
         uletters = [chr(i) for i in range(65, 91)]
         wpmcounter = curses.newwin(1, curses.COLS, y-2, x-len(sentence)//2)        
         pos = 0
+        posy = 0
         cch = 0
         ich = 0
 
@@ -183,28 +185,44 @@ def main(window):
         window.addstr(y, x, sentence)
         window.addstr(y, x, sentence[pos], curses.A_UNDERLINE)
         window.addstr(y, x+1, sentence[pos+1], curses.A_BOLD)
+        window.addstr(y+1, x, s1)
+        window.addstr(y+2, x, s2)
         window.move(y, x)
-        curses.echo()
+        curses.noecho()
         curses.curs_set(0)
+        end_sentence = False
         while True:
             letter = window.getkey()
+            if pos == len(sentence) and posy == len(sentences)-1:
+                break
             # CHECK LETTER (CASE 1: correct letter, incorrect letter)
             if letter in lletters or letter in uletters or letter == chr(32) or letter == chr(39):
                 #START AT FIRST LETTER
-                if pos == 0:
+                if pos == 0 and posy == 0:
                     start = time.time()
-                # CASE 3: END OF SENTENCE
-                if pos > len(sentence)-1:
-                    break
+                # CASE 2: END OF SENTENCE
+                if pos == len(sentence):
+                    y += 1
+                    x -= pos
+                    pos = 0
+                    end_sentence = True
+                    if posy == 2:
+                        break
+                    elif posy == 1:
+                        sentence = s2
+                        posy = 2
+                    elif posy == 0:
+                        sentence = s1
+                        posy = 1
                 # CORRECT
-                if letter == sentence[pos]:
-                    stops[pos] = 1
+                if letter == sentence[pos] and end_sentence == False:
+                    stops[posy][pos] = 1
                     window.addstr(y, x, sentence[pos], green)
                     x += 1
                     pos += 1
                     cch += 1
                 # INCORRECT
-                elif letter != sentence[pos]:
+                elif letter != sentence[pos]and end_sentence == False:
                     if sentence[pos] == ' ':
                         window.addstr(y, x, letter, magenta)
                         x += 1
@@ -215,23 +233,32 @@ def main(window):
                         x += 1
                         pos += 1
                         ich += 1
-            # CASE 2: BACKSPACE
+            # CASE 3: BACKSPACE
             elif letter == 'KEY_BACKSPACE' and pos > 0:
-                if pos > len(sentence)-1:
-                    break
+                if pos == len(sentence):
+                    window.addstr(y, x-1, sentence[pos-1])
+                    x -= 1
+                    pos -= 1
                 else:
-                    if stops[pos-1] == 0:
+                    if stops[posy][pos-1] == 0:
                         window.addstr(y, x-1, sentence[pos-1])
                         x -= 1
                         pos -= 1
-                    elif stops[pos-1] == 1:
-                        stops[pos-1] = 0
+                    elif stops[posy][pos-1] == 1:
+                        stops[posy][pos-1] = 0
                         window.addstr(y, x-1, sentence[pos-1])
                         x -= 1
                         pos -= 1
-            # CASE 3: END OF SENTENCE
-            elif pos > len(sentence)-1:
-                break
+            elif letter == 'KEY_BACKSPACE' and pos == 0 and posy > 0:
+                window.addstr(y, x, sentence[pos])
+                y -= 1
+                posy -= 1
+                sentence = sentences[posy]
+                pos = len(sentence)
+                x += len(sentence)
+
+            
+            
             # UPDATE
             if pos > 1:
                 seconds = (time.time()-start)
@@ -239,13 +266,13 @@ def main(window):
                 wps = cchpersecond/5
                 wpm = wps*60
                 accuracy = (cch/(cch+ich))*100
-                realwpm = (len(sentence.split())/seconds)*60
+                realwpm = (len(sentence+s1+s2)/seconds)*60
                 updatewpm(wpmcounter, wpm, accuracy)
             if pos < len(sentence):
                 window.addstr(y, x, sentence[pos], curses.A_UNDERLINE)
-            if pos < len(sentence)-1:
-                window.addstr(y, x, sentence[pos], curses.A_UNDERLINE)
-                window.addstr(y, x+1, sentence[pos+1], curses.A_BOLD)
+                if pos < len(sentence)-1:
+                    window.addstr(y, x+1, sentence[pos+1], curses.A_BOLD)
+            end_sentence = False
             window.refresh()
             window.move(y, x)
 
