c73be6e113354a80302981d4789ea951eeaf62a1
Ian Shapiro <ianshapiro1@gmail.com>
2025-05-28 17:40:26 -0500

    refactor typing logic

--- core/logic.py
+++ core/logic.py
@@ -43,32 +43,29 @@ def handle_backspace(window, y, x, posx, stops, sentence, realcch, realich):
     return x - 1, posx - 1, realcch, realich
 
 
-def paragraph_mode(window):
-    max_y, max_x = window.getmaxyx()
-    window.nodelay(True)
-    window.timeout(100)
-
-    center_y, center_x = max_y // 2 - 2, max_x // 4
-    col_width = max_x - 2 * center_x
-
-    sentence_list = [generate_sentence(col_width) for _ in range(3)]
-    sentence = sentence_list[0]
+def typing_loop(window, sentences, center_y, center_x, is_paragraph):
+    col_width = curses.COLS - 2 * center_x
     posx = posy = 0
     x_offset = center_x
-    wordlen = avg_word_length((" ".join(sentence_list)).split())
+    sentence = sentences[0]
+    wordlen = avg_word_length(" ".join(sentences).split())
 
-    wpm = acc = seconds = cch = ich = realcch = realich = 0
-    stops = [[0 for _ in s] for s in sentence_list]
-    wpmcounter = curses.newwin(1, curses.COLS, center_y - 2, x_offset)
+    cch = ich = realcch = realich = 0
+    stops = [[0 for _ in s] for s in sentences]
+    wpm = acc = seconds = 0
+    start = None
+    wpmcounter = curses.newwin(1, curses.COLS, center_y - 2, center_x)
 
+    window.nodelay(True)
+    window.timeout(100)
     curses.noecho()
     curses.curs_set(0)
     window.clear()
 
-    draw_sentence_lines(window, sentence_list, center_y, x_offset)
-    window.addstr(center_y + 4, x_offset, 'Exit(home/del) | New(enter)', curses.color_pair(int(config.config_parser['default']['colorsession'])))
-    highlight_characters(window, center_y, x_offset, sentence, posx)
-    window.move(center_y, x_offset)
+    draw_sentence_lines(window, sentences, center_y, center_x)
+    window.addstr(center_y + (len(sentences) + 1), center_x, 'Exit(home/del) | New(enter)', curses.color_pair(int(config.config_parser['default']['colorsession'])))
+    highlight_characters(window, center_y, center_x, sentence, posx)
+    window.move(center_y, center_x)
     window.refresh()
 
     while True:
@@ -84,39 +81,54 @@ def paragraph_mode(window):
             if posx == 0 and posy == 0:
                 start = time.time()
 
-            if posx == len(sentence):
-                center_y += 1
-                x_offset -= posx
-                posx = 0
-                posy += 1
-                sentence = sentence_list[posy]
-                continue
+            if posx >= len(sentence):
+                if is_paragraph and posy < len(sentences) - 1:
+                    center_y += 1
+                    x_offset -= posx
+                    posx = 0
+                    posy += 1
+                    sentence = sentences[posy]
+                    continue
+                else:
+                    break
 
-            if letter == sentence[posx]:
-                stops[posy][posx] = 1
-                color = curses.color_pair(int(config.config_parser['default']['colorcorrect']))
-                cch += 1; realcch += 1
+            expected_char = sentence[posx]
+            is_correct = letter == expected_char
+            color_correct = curses.color_pair(int(config.config_parser['default']['colorcorrect']))
+            color_incorrect = curses.color_pair(int(config.config_parser['default']['colorincorrect']))
+            stops[posy][posx] = 1 if is_correct else 0
+
+            if is_correct:
+                cch += 1
+                realcch += 1
+                window.addch(center_y, x_offset, expected_char, color_correct)
             else:
-                color = curses.color_pair(int(config.config_parser['default']['colorincorrect']))
-                ich += 1; realich += 1
+                ich += 1
+                realich += 1
+                if expected_char == ' ' and letter != ' ':
+                    window.addch(center_y, x_offset, letter, color_incorrect)
+                elif expected_char != ' ' and letter == ' ':
+                    window.addch(center_y, x_offset, expected_char, color_incorrect)
+                else:
+                    window.addch(center_y, x_offset, letter, color_incorrect)
+            x_offset += 1
+            posx += 1
 
-            window.addch(center_y, x_offset, sentence[posx] if sentence[posx] != ' ' or letter != sentence[posx] else letter, color)
-            x_offset += 1; posx += 1
-            if posx == len(sentence) and posy == 2:
+            if posx >= len(sentence) and posy == len(sentences) - 1:
                 break
 
         elif letter == 'KEY_BACKSPACE':
             if posx > 0:
                 x_offset, posx, realcch, realich = handle_backspace(window, center_y, x_offset, posx, stops[posy], sentence, realcch, realich)
             elif posx == 0 and posy > 0:
-                center_y -= 1
                 posy -= 1
-                sentence = sentence_list[posy]
+                sentence = sentences[posy]
                 posx = len(sentence)
+                center_y -= 1
                 x_offset += posx
 
         elif letter == '\n':
-            paragraph_mode(window)
+            return sentence_mode(window) if not is_paragraph else paragraph_mode(window)
 
         if posx > 1 and posx != len(sentence) and letter in constants.LETTERS + constants.SPECIAL_CHARS:
             seconds = time.time() - start
@@ -128,76 +140,20 @@ def paragraph_mode(window):
         window.move(center_y, x_offset)
 
     window.nodelay(False)
-    ui.displayinfo(window, wpm, acc, seconds, realcch, realich, cch, ich, 2)
+    ui.displayinfo(window, wpm, acc, seconds, realcch, realich, cch, ich, 2 if is_paragraph else 1)
+
+
+def paragraph_mode(window):
+    max_y, max_x = window.getmaxyx()
+    center_y, center_x = max_y // 2 - 2, max_x // 4
+    col_width = max_x - 2 * center_x
+    sentences = [generate_sentence(col_width) for _ in range(3)]
+    typing_loop(window, sentences, center_y, center_x, is_paragraph=True)
 
 
 def sentence_mode(window):
     max_y, max_x = window.getmaxyx()
     center_y, center_x = max_y // 2, max_x // 4
     col_width = max_x - 2 * center_x
-
     sentence = generate_sentence(col_width)
-    posx = 0
-    wordlen = avg_word_length(sentence.split())
-    stops = [0 for _ in sentence]
-
-    wpm = acc = seconds = cch = ich = realcch = realich = 0
-    wpmcounter = curses.newwin(1, curses.COLS, center_y - 2, center_x)
-
-    window.nodelay(True)
-    window.timeout(100)
-    window.clear()
-
-    curses.noecho()
-    curses.curs_set(0)
-    window.addstr(center_y, center_x, sentence)
-    highlight_characters(window, center_y, center_x, sentence, posx)
-    window.addstr(center_y + 2, center_x, 'Exit(home/del) | New(enter)', curses.color_pair(int(config.config_parser['default']['colorsession'])))
-    window.move(center_y, center_x)
-
-    while True:
-        try:
-            letter = window.getkey()
-        except:
-            letter = None
-
-        if letter is not None and (letter in constants.LETTERS or letter in " '-"):
-            if posx == 0:
-                start = time.time()
-
-            if posx >= len(sentence):
-                break
-
-            if letter == sentence[posx]:
-                stops[posx] = 1
-                color = curses.color_pair(int(config.config_parser['default']['colorcorrect']))
-                cch += 1; realcch += 1
-            else:
-                color = curses.color_pair(int(config.config_parser['default']['colorincorrect']))
-                ich += 1; realich += 1
-
-            window.addch(center_y, center_x, sentence[posx] if sentence[posx] != ' ' or letter != sentence[posx] else letter, color)
-            center_x += 1; posx += 1
-            if posx >= len(sentence) and letter:
-                break
-
-        elif letter == 'KEY_BACKSPACE' and posx > 0:
-            center_x, posx, realcch, realich = handle_backspace(window, center_y, center_x, posx, stops, sentence, realcch, realich)
-
-        elif letter in ('KEY_HOME', 'KEY_DC'):
-            ui.menu(window)
-
-        elif letter == '\n':
-            sentence_mode(window)
-
-        if posx > 1 and posx != len(sentence) and letter in constants.LETTERS + constants.SPECIAL_CHARS:
-            seconds = time.time() - start
-            wpm = (cch / seconds / max(1, wordlen)) * 60
-            acc = (cch / (cch + ich)) * 100
-            ui.updatewpm(wpmcounter, wpm, acc)
-
-        highlight_characters(window, center_y, center_x, sentence, posx)
-        window.move(center_y, center_x)
-
-    window.nodelay(False)
-    ui.displayinfo(window, wpm, acc, seconds, realcch, realich, cch, ich, 1)
+    typing_loop(window, [sentence], center_y, center_x, is_paragraph=False)
